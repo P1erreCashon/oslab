13721edf33d1bbdc80c39c4c3710d5d0e6cccaa0
zzy <Adris_dreams@outlook.com>
2025-09-07 10:41:30 +0800

    types.h 添加只编译一次宏定义

--- src/types.h
+++ src/types.h
@@ -1,3 +1,5 @@
+#ifndef TYPES_H
+#define TYPES_H
 typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
@@ -9,8 +11,8 @@ typedef unsigned long uint64;
 
 typedef uint64 pde_t;
 
-// 时间结构体，用于 gettimeofday 系统调用
 struct timeval {
   uint64 tv_sec;   // 秒数
   uint64 tv_usec;  // 微秒数
 };
+#endif // TYPES_H
